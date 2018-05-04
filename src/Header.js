3f6857842a08a3bb50926abc2d2ebc70cc6f0227
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-05-04 08:35:33 +0200

    prettier

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,41 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
 import './App.css';
 
 class Header extends Component {
   render() {
     return (
-        <div
-            style={{
-                position: "absolute",
-                width: '100%',
-                height: '10%',
-                top: '10px',
-            }}
+      <div
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '10%',
+          top: '10px'
+        }}
+      >
+        <h1
+          style={{
+            fontFamily: 'Damion',
+            color: '#FD46F5',
+            textAlign: 'center',
+            verticalAlign: 'center',
+            lineHeight: '100%',
+            fontSize: '55px'
+          }}
         >
-            <h1
-            style={{
-                fontFamily: "Damion",
-                color: '#FD46F5',
-                textAlign: 'center',
-                verticalAlign: 'center',
-                lineHeight: '100%',
-                fontSize: '55px'
-            }}
-            ><i>Klokr</i></h1>
-            <i 
-                className="material-icons"
-                style={{
-                    position: 'absolute',
-                    top: '20px',
-                    right: '20px'
-                }}
-            >
-                settings
-            </i>
-        </div>
-    );  
+          <i>Klokr</i>
+        </h1>
+        <i
+          className="material-icons"
+          style={{
+            position: 'absolute',
+            top: '20px',
+            right: '20px'
+          }}
+        >
+          settings
+        </i>
+      </div>
+    );
   }
 }
 
